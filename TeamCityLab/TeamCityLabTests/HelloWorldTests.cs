005053a870aad4749d70b920c6780ac38570d2e9
Simon Evertsson <simon.evertsson@valtech.se>
2015-02-09 14:08:04 +0100

    Added unittests

---
+++ TeamCityLab/TeamCityLabTests/HelloWorldTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TeamCityLabTests
+{
+    [TestFixture]
+    class HelloWorldTests
+    {
+
+        [Test]
+        public void should_pass()
+        {
+            Assert.AreEqual(1,1);
+        }
+
+        [Test]
+        public void should_not_pass()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
